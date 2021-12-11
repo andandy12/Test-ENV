1b5d7dfcfef997a0e31f13d7e506a66945fb11cb
andandy12 <30943710+andandy12@users.noreply.github.com>
2021-12-11 15:17:47 -0500

    Add files via upload

---
+++ StopPreview/StopStreamPreview.plugin.js
@@ -0,0 +1,42 @@
+/**
+ * @name StopPreview
+ */
+module.exports = class OTRClass {
+    cancelMakeChunkRequestPatch = () => { };
+    getName() { return "Stop Preview"; };
+    getDescription() { return "This will force all preview post requests to be malformed."; };
+    getVersion() { return "0.0.1"; };
+    getAuthor() { return "andandy12"; };
+
+    start() {
+        BdApi.showToast("Stop Preview is starting");
+        console.log("\n[Stop Preview] Starting");
+
+        this.makeChunkRequest();
+    }
+    /**
+     * Patches sendMessage so we can intercept messages sent.
+     */
+     makeChunkRequest() {
+        console.log("[Stop Preview] Patching makeChunkRequest()");
+        this.cancelMakeChunkRequestPatch = BdApi.monkeyPatch(BdApi.findModuleByProps("makeChunkedRequest"), "makeChunkedRequest", {
+            once: false, before: (e) => {
+                console.log(e.methodArguments);
+                console.log(e.methodArguments[0].includes("preview") && e.methodArguments[2].method == "POST");
+                if (e.methodArguments[0].includes("preview") && e.methodArguments[2].method == "POST") {
+                    e.methodArguments[2].method = "GET"; // force the post to be a get as a failsafe
+                    e.methodArguments[1].thumbnail == "";// change the thumbnail to be an empty image
+                }
+            }
+        })
+    }
+
+
+    stop() {
+        console.log("[Stop Preview] Unpatching makeChunkRequest()");
+        this.cancelMakeChunkRequestPatch();
+
+        console.log("[Stop Preview] Stopped");
+        BdApi.showToast("[Stop Preview] Stopped");
+    }
+}
