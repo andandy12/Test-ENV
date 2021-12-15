c5c8d3f82f3fe0ff8e4a257bb3ea3b9393f075c9
andandy12 <30943710+andandy12@users.noreply.github.com>
2021-12-15 03:08:07 -0500

    Update StopStreamPreview.plugin.js

--- StopPreview/StopStreamPreview.plugin.js
+++ StopPreview/StopStreamPreview.plugin.js
@@ -1,72 +1,46 @@
 /**
- * @name UnlockStreamQuality
+ * @name StopMyPreview
  */
  module.exports = class OTRClass {
-    originalRequirements = [];
-    originalBoostedGuildFeatures = [];
-    cancelFakeDeafen = ()=>{};
-
-    getName() { return "Stream Settings Unlocked"; };
-    getDescription() { return "This will unlock stream settings removing their requirments."; };
-    getVersion() { return "0.0.1"; };
+    cancelMakeChunkedRequestPatch = () => { };
+    getName() { return "Stop My Preview"; };
+    getDescription() { return "This will force all preview post requests to not happen."; };
+    getVersion() { return "0.0.2"; };
     getAuthor() { return "andandy12"; };
 
     start() {
-        BdApi.showToast("Stream Settings Unlocked is starting");
-        console.log("\n[Stream Settings Unlocked] Starting");
+        BdApi.showToast("Stop My Preview is starting");
+        console.log("\n[Stop My Preview] Starting");
 
-        //deletes userPremiumType and guildPremiumTier from the requirments array.
-        this.originalRequirements = BdApi.findModuleByProps("ApplicationStreamPresets").ApplicationStreamSettingRequirements;
-        for(let i =0; i<BdApi.findModuleByProps("ApplicationStreamPresets").ApplicationStreamSettingRequirements.length;i++){
-            delete BdApi.findModuleByProps("ApplicationStreamPresets").ApplicationStreamSettingRequirements[i].userPremiumType;
-            delete BdApi.findModuleByProps("ApplicationStreamPresets").ApplicationStreamSettingRequirements[i].guildPremiumTier;
-        }
-        
-        // (sometimes when in bulk) allows you to upload more emojis than allowed... they cant be used until you have the proper level
-        let a = BdApi.findModuleByProps("AppliedGuildBoostsRequiredForBoostedGuildTier").BoostedGuildFeatures[3];
-        this.originalBoostedGuildFeatures = BdApi.findModuleByProps("AppliedGuildBoostsRequiredForBoostedGuildTier").BoostedGuildFeatures;
-        let temp = [];temp[0] = a;temp[1] = a;temp[2] = a;temp[3] = a;
-        BdApi.findModuleByProps("AppliedGuildBoostsRequiredForBoostedGuildTier").BoostedGuildFeatures = temp;
-        
-        // lets you talk in vcs before 10 minutes has big potential to break in future
-        BdApi.findModuleByProps("AppliedGuildBoostsRequiredForBoostedGuildTier").VerificationCriteria = {ACCOUNT_AGE: 5, MEMBER_AGE: 0};
-        
-        //Pretty self explainatory... this took long really long to find, but its worth as I didn't want to mod the websocket directly
-        this.cancelFakeDeafen = BdApi.monkeyPatch(BdApi.findModuleByPrototypes("lobbyConnect").prototype, "voiceStateUpdate", {
-            instead: (e) => {
-                e.callOriginalMethod();
-                if(e.methodArguments[3] == true) {
-                    BdApi.showConfirmationModal(`plugin`, "Do you want to do a fake deafen?", {
-                        cancelText: "No",
-                        confirmText: "Yes",
-                        onConfirm: () => { 
-                            BdApi.findModuleByProps("toggleSelfDeaf").toggleSelfDeaf();
-                            setTimeout(() => {
-                                e.thisObject.send(4,{
-                                    guild_id: e.methodArguments[0],
-                                    channel_id: e.methodArguments[1],
-                                    self_mute: true,
-                                    self_deaf: true,
-                                    self_video: e.methodArguments[4]
-                                })
-                            }, 250);
-                        }
-                    });
+        this.patchmakeChunkedRequest();
+    }
+    /**
+     * Patches makeChunkRequest so we can stop preview post request from happening
+     */
+    patchmakeChunkedRequest() {
+        console.log("[Stop My Preview] Patching makeChunkedRequest()");
+        this.cancelMakeChunkedRequestPatch = BdApi.monkeyPatch(BdApi.findModuleByProps("makeChunkedRequest"), "makeChunkedRequest", {
+            once: false, 
+            /*before: (e) => { // this one fails but we waste bandwidth as it will retry every so often
+                if (e.methodArguments[0].includes("preview") && e.methodArguments[2].method == "POST") {
+                    e.methodArguments[2].method = "GET"; // force the post to be a get as a failsafe
+                    return e.methodArguments[1].thumbnail == "";// change the thumbnail to be an empty image
+                }
+            }*/
+            instead: (e) => {//if its a preview post req we do nothing
+                if (!e.methodArguments[0].includes("preview") && !e.methodArguments[2].method == "POST") {
+                    e.callOriginalMethod();
                 }
             }
-        });
+        })
     }
+
 
     stop() {
-        BdApi.findModuleByProps("ApplicationStreamPresets").ApplicationStreamSettingRequirements = this.originalRequirements;
-        this.cancelFakeDeafen();
-        BdApi.findModuleByProps("ApplicationStreamPresets").VerificationCriteria = {ACCOUNT_AGE: 5, MEMBER_AGE: 10}
-        BdApi.findModuleByProps("AppliedGuildBoostsRequiredForBoostedGuildTier").BoostedGuildFeatures = this.originalBoostedGuildFeatures;
-        console.log("[Stream Settings Unlocked] Stopped");
-        BdApi.showToast("[Stream Settings Unlocked] Stopped");
+        console.log("[Stop My Preview] Unpatching makeChunkedRequest()");
+        this.cancelMakeChunkedRequestPatch();
+
+        console.log("[Stop My Preview] Stopped");
+        BdApi.showToast("[Stop My Preview] Stopped");
     }
 }
-
-// things attempted that did not work 
-//  patching getUserMaxFileSize so it returns 85899345920;
-//      discord would get mad at end of upload...
