949a2e04cf992cd1c1836775cda3a51c4c47a740
andandy12 <30943710+andandy12@users.noreply.github.com>
2022-02-17 04:57:33 -0500

    Investigated analyticsTrackingStoreMaker
    We can not block science requests on the client via the line below nothing has not worked so far...
    BdApi.findModuleByProps("analyticsTrackingStoreMaker").AnalyticsActionHandlers.handleTrack = ()=>{}

--- BetterDiscord/doc.js
+++ BetterDiscord/doc.js
@@ -1,1 +1,12 @@
+// This is very interesting...
+BdApi.findModuleByProps("analyticsTrackingStoreMaker")
 
+BdApi.findModuleByProps("analyticsTrackingStoreMaker").AnalyticsActionHandlers.handleTrack // Will need to test patching and to see if science stops or if anything breaks
+  //2/17/21 - appears to be what is called when we send science requests will block most likely
+    BdApi.findModuleByProps("analyticsTrackingStoreMaker").AnalyticsActionHandlers.handleTrack = ()=>{}; // Works to block science/tracking requests no adverse affects seen so far
+BdApi.findModuleByProps("analyticsTrackingStoreMaker").AnalyticsActionHandlers.handleFingerprint // This seems crucial to verifying phone as well as other things
+
+BdApi.findModuleByProps("addPhoneWithoutPassword").validatePhoneForSupport(/*discord token*/) // returns 400 | {token: ["Invalid token"]} 
+BdApi.findModuleByProps("addPhoneWithoutPassword").validatePhoneForSupport("asd") // returns 500| {message: "500: Internal Server Error", code: 0}
+
+BdApi.findModuleByProps("analyticsTrackingStoreMaker")
